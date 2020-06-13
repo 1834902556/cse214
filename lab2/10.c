91e8150147a2896449208e509e67a54bc1f4c562
NAJMUL ISLAM <65591333+1834902556@users.noreply.github.com>
2020-06-13 15:01:10 +0600

    Update 10.c

--- lab2/10.c
+++ lab2/10.c
@@ -1,22 +1,22 @@
-/*10.Delete a value “x” (take input x from user) from the array shift all other value to left. If “X”
+/*10.Delete a value â€œxâ€ (take input x from user) from the array shift all other value to left. If â€œXâ€
     is not present in the given array print Element "Not Found". Only delete the first occurrence of
     the value. And print the whole array. */
 #include<stdio.h>
 int main ()
 {
-    int array[20]= {1,2,3,4,5,6,7,8,9,10};
+    int arr[20]= {1,2,3,4,5,6,7,8,9,10};
     int i, j, x, tag=0,size=10;
     scanf("%d",&x);
     for(i=0; i<size; i++){
-        printf("%d ",array[i]);
+        printf("%d ",arr[i]);
     }
     printf("\n");
     for( j=0; j<size; j++){
-        if(x==array[j])
+        if(x==arr[j])
         {
             for( i=x-1; i<=size; i++)
             {
-                array[i] = array[i+1];
+                arr[i] = arr[i+1];
             }
             tag++;
         }
@@ -28,7 +28,7 @@ int main ()
     else
     {
         for( i=0; i<size; i++){
-            printf("%d ",array[i]);
+            printf("%d ",arr[i]);
         }
     }
 
