3c1d350310b1d744519cef887829ca6219ee0542
NAJMUL ISLAM <65591333+1834902556@users.noreply.github.com>
2020-07-18 20:58:50 +0600

    Update 1.c

--- lab10/1.c
+++ lab10/1.c
@@ -24,13 +24,13 @@ void main()
     do
     {
         system("cls");
-        printf(" 1 : push\n");
-        printf(" 2 : pop\n");
-        printf(" 3 : Top\n");
-        printf(" 4 : Empty\n");
-        printf(" 5 : Display\n");
-        printf(" 6 : Queue size\n");
-        printf(" 7 : Exit\n");
+        printf(" 1 - push\n");
+        printf(" 2 - pop\n");
+        printf(" 3 - Top\n");
+        printf(" 4 - Empty\n");
+        printf(" 5 - Display\n");
+        printf(" 6 - Queue size\n");
+        printf(" 7 - Exit\n");
         printf("Enter choice : ");
         scanf("%d", &op);
         if( op == 1)
